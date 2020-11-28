33f3aa90afa8cf92e9972961682c4baf11a8573b
jakemercure28 <52304691+jakemercure28@users.noreply.github.com>
2020-11-27 22:57:09 -0800

    Add files via upload

---
+++ Auto Reservation System.aspx
@@ -0,0 +1,235 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Auto Reservation System.aspx.vb" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 74%;
+            height: 607px;
+        }
+        .auto-style2 {
+            text-align: center;
+        }
+        .auto-style3 {
+            width: 180px;
+        }
+        .auto-style12 {
+            height: 42px;
+            width: 180px;
+            text-align: center;
+            background-color: #CCFFCC;
+        }
+        .auto-style13 {
+            height: 36px;
+            width: 180px;
+            text-align: center;
+            background-color: #CCFFCC;
+        }
+        .auto-style14 {
+            width: 180px;
+            height: 80px;
+            text-align: center;
+        }
+        .auto-style16 {
+            width: 180px;
+            height: 33px;
+            text-align: center;
+            background-color: #CCFFCC;
+        }
+        .auto-style18 {
+            width: 180px;
+            height: 34px;
+            text-align: center;
+            background-color: #CCFFCC;
+        }
+        .auto-style21 {
+            text-align: center;
+            width: 99px;
+        }
+        .auto-style22 {
+            height: 42px;
+            background-color: #CCFFCC;
+            width: 99px;
+        }
+        .auto-style23 {
+            height: 36px;
+            background-color: #CCFFCC;
+            width: 99px;
+        }
+        .auto-style24 {
+            height: 33px;
+            background-color: #CCFFCC;
+            width: 99px;
+        }
+        .auto-style25 {
+            height: 34px;
+            text-align: left;
+            background-color: #CCFFCC;
+            width: 99px;
+        }
+        .auto-style26 {
+            height: 80px;
+            text-align: left;
+            width: 99px;
+            background-color: #CCFFCC;
+        }
+        .auto-style27 {
+            height: 80px;
+            text-align: right;
+            width: 99px;
+            background-color: #CCFFCC;
+        }
+        .auto-style28 {
+            width: 99px;
+        }
+        .auto-style29 {
+            height: 42px;
+            background-color: #CCFFCC;
+            width: 12px;
+        }
+        .auto-style30 {
+            height: 36px;
+            background-color: #CCFFCC;
+            width: 12px;
+        }
+        .auto-style31 {
+            height: 33px;
+            background-color: #CCFFCC;
+            width: 12px;
+        }
+        .auto-style32 {
+            height: 34px;
+            text-align: left;
+            background-color: #CCFFCC;
+            width: 12px;
+        }
+        .auto-style33 {
+            height: 80px;
+            text-align: left;
+            width: 12px;
+        }
+        .auto-style34 {
+            height: 80px;
+            text-align: right;
+            width: 12px;
+        }
+        .auto-style35 {
+            width: 12px;
+        }
+        .auto-style36 {
+            text-align: left;
+            color: #0000FF;
+        }
+        .auto-style37 {
+            color: #0000FF;
+        }
+        .auto-style38 {
+            width: 99px;
+            background-color: #CCFFCC;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2" colspan="2">WSU Auto Reservation System</td>
+                <td class="auto-style21">&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style12">Start Date: (mm/dd/yyyy)</td>
+                <td class="auto-style29">
+                    <asp:TextBox ID="TextBox1" runat="server" AutoPostBack="True"></asp:TextBox>
+                </td>
+                <td class="auto-style22">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style13">End Date: (mm/dd/yyyy)</td>
+                <td class="auto-style30">
+                    <asp:TextBox ID="TextBox2" runat="server" AutoPostBack="True"></asp:TextBox>
+                </td>
+                <td class="auto-style23">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style16">Start Mileage:</td>
+                <td class="auto-style31">
+                    <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style24">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style18">End Mileage:</td>
+                <td class="auto-style32">
+                    <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                </td>
+                <td class="auto-style25">
+                    &nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style14">Select Vehicle Type:</td>
+                <td class="auto-style33">
+                    <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True">
+                        <asp:ListItem Value="Sedan1">Sedan1</asp:ListItem>
+                        <asp:ListItem Value="Sedan2">Sedan2</asp:ListItem>
+                        <asp:ListItem Value="Sedan3">Sedan3</asp:ListItem>
+                        <asp:ListItem Value="Van1">Van1</asp:ListItem>
+                        <asp:ListItem Value="Van2">Van2</asp:ListItem>
+                        <asp:ListItem Value="Van3">Van3</asp:ListItem>
+                        <asp:ListItem Value="Pickup1">Pickup1</asp:ListItem>
+                        <asp:ListItem Value="Pickup2">Pickup2</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+                <td class="auto-style26">
+                    <span class="auto-style37">Grand Totals</span><asp:GridView ID="GridView3" runat="server">
+                    </asp:GridView>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style14">Additional Costs (Optional):</td>
+                <td class="auto-style34">
+                    <asp:CheckBoxList ID="CheckBoxList1" runat="server" AutoPostBack="True" RepeatDirection="Horizontal" Width="238px">
+                        <asp:ListItem Value="25">Light cleaning</asp:ListItem>
+                        <asp:ListItem Value="75">Heavy cleaning</asp:ListItem>
+                        <asp:ListItem Value="150">Light damage</asp:ListItem>
+                        <asp:ListItem Value="300">Heavy damage</asp:ListItem>
+                        <asp:ListItem Value="25">Late fee</asp:ListItem>
+                    </asp:CheckBoxList>
+                </td>
+                <td class="auto-style27">
+                    <div class="auto-style36">
+                        Vehicle Type Usage</div>
+                    <asp:GridView ID="GridView4" runat="server">
+                    </asp:GridView>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">
+                    <asp:Button ID="Button1" runat="server" Text="Create Invoice" />
+                </td>
+                <td class="auto-style35">
+                    <asp:TextBox ID="TextBox3" runat="server" Height="117px" TextMode="MultiLine" Width="420px"></asp:TextBox>
+                </td>
+                <td class="auto-style38">
+                    <span class="auto-style37">Individual Vehicle Usage</span><asp:GridView ID="GridView2" runat="server">
+                    </asp:GridView>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">&nbsp;</td>
+                <td class="auto-style35">
+                    <span class="auto-style37">Invoice Ledger</span><asp:GridView ID="GridView1" runat="server">
+                    </asp:GridView>
+                </td>
+                <td class="auto-style28">
+                    &nbsp;</td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ Auto Reservation System.aspx.vb
@@ -0,0 +1,241 @@
+﻿Imports System.Data
+Partial Class _Default
+    Inherits System.Web.UI.Page
+    Public Shared gdtRentals, individual, vehicleType, grandTotal As New DataTable
+
+    'Data table initialization
+    Private Sub Rentals(sender As Object, e As EventArgs) Handles Me.Init
+
+
+        If individual.Columns.Count > 0 Then Exit Sub
+        If vehicleType.Columns.Count > 0 Then Exit Sub
+        If grandTotal.Columns.Count > 0 Then Exit Sub
+
+
+
+        'invoice ledger initialization
+        With gdtRentals.Columns
+            .Add("Invoice ID", GetType(Integer))
+            .Add("Checkout Date", GetType(String))
+            .Add("Checkin Date", GetType(String))
+            .Add("Checkout Mileage", GetType(Integer))
+            .Add("Checkin Mileage", GetType(Integer))
+            .Add("Vehicle Type", GetType(String))
+            .Add("Total Charges", GetType(Integer))
+            .Add("Reservation Total Count", GetType(Integer))
+        End With
+
+        'auto incrementing for each invoice id
+        With gdtRentals.Columns("Invoice ID")
+            .AutoIncrement = True
+            .AutoIncrementSeed = 10000
+            .AutoIncrementStep = 1
+        End With
+
+        'set running total for reservations
+        gdtRentals.Columns("Reservation Total Count").DefaultValue = 0
+
+
+
+        'individual vehicle data table initialization
+        With individual.Columns
+            .Add("Vehicle ID", GetType(String))
+            .Add("Current Mileage", GetType(Integer))
+            .Add("Needs Maintainence", GetType(Boolean))
+        End With
+
+        For Each li As ListItem In DropDownList1.Items
+            Dim dr2 As DataRow = individual.NewRow
+            dr2.Item("Vehicle ID") = li.Text
+            individual.Rows.Add(dr2)
+        Next
+
+        GridView2.DataSource = individual
+        GridView2.DataBind()
+
+
+
+        'vehicle type initialization
+        With vehicleType
+            .Columns.Add("Vehicle Type", GetType(String))
+            .Columns.Add("Total Mileage", GetType(Integer))
+
+            .Columns("Total Mileage").DefaultValue = 0
+        End With
+
+        Dim dr3 As DataRow = vehicleType.NewRow
+        Dim dr4 As DataRow = vehicleType.NewRow
+        Dim dr5 As DataRow = vehicleType.NewRow
+        dr3.Item("Vehicle Type") = "Sedan"
+        vehicleType.Rows.Add(dr3)
+        dr4.Item("Vehicle Type") = "Van"
+        vehicleType.Rows.Add(dr4)
+        dr5.Item("Vehicle Type") = "Pickup"
+        vehicleType.Rows.Add(dr5)
+
+        GridView4.DataSource = vehicleType
+        GridView4.DataBind()
+
+
+        'grand total initialization
+        With grandTotal
+            .Columns.Add("Total Times Rented", GetType(Integer))
+            .Columns.Add("Total Days Rented", GetType(Integer))
+            .Columns.Add("Total Miles Traveled", GetType(Integer))
+            .Columns.Add("Total Revenue Generated", GetType(Decimal))
+
+            .Columns("Total Times Rented").DefaultValue = 0
+            .Columns("Total Days Rented").DefaultValue = 0
+            .Columns("Total Miles Traveled").DefaultValue = 0
+            .Columns("Total Revenue Generated").DefaultValue = 0
+        End With
+
+        Dim dr As DataRow = grandTotal.NewRow
+        grandTotal.Rows.Add(dr)
+
+        GridView3.DataSource = grandTotal
+        GridView3.DataBind()
+
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        'input error checking
+        If TextBox1.Text = Nothing OrElse TextBox2.Text = Nothing Then
+            TextBox3.Text = "Check entries for time and date."
+            Exit Sub
+        End If
+
+        If TextBox4.Text = Nothing OrElse TextBox5.Text = Nothing Then
+            TextBox3.Text = "Check entries for mileage."
+            Exit Sub
+        End If
+
+        If DropDownList1.SelectedIndex = -1 Then
+            TextBox3.Text = "Please select a vehicle type."
+            Exit Sub
+        End If
+
+
+        'We are going to be using TextMode for textbox 4,5 that inputs as text in the form "MM/DD/YYYY"
+        'because of a non funtioning "date" textbox functionality. It will then be converted to a date to then be processed
+        Dim begDate = Convert.ToDateTime(TextBox1.Text)
+        Dim endDate = Convert.ToDateTime(TextBox2.Text)
+        Dim ts As TimeSpan = endDate - begDate
+
+        'check if date is invalid
+        If ts.TotalDays < 0 Then
+            TextBox3.Text = "Please check date entries."
+            Exit Sub
+        End If
+
+        If begDate.Date > Today.Date OrElse endDate.Date > Today.Date Then
+            TextBox3.Text = "Please check date entries."
+            Exit Sub
+        End If
+
+        'Integer data variables
+        Dim totalMileage As Integer = 0
+        Dim additionalCharges As Integer = 0
+        Dim mileageCost As Integer = 0
+        Dim totalCost As Integer = 0
+        Dim pricePerDay As Integer = 0
+
+        'sedan 0,1,2 van 3,4,5 pickup 6,7
+        If (DropDownList1.SelectedIndex = 0 Or 1 Or 2) Then
+            pricePerDay = 30
+        End If
+        If (DropDownList1.SelectedIndex = 3 Or 4 Or 5) Then
+            pricePerDay = 70
+        End If
+        If (DropDownList1.SelectedIndex = 6 Or 7) Then
+            pricePerDay = 90
+        End If
+
+        'calculate mileage charge
+        totalMileage = TextBox5.Text - TextBox4.Text
+        mileageCost = (0.25 * totalMileage)
+
+        'calculate extra charges
+        Dim item As ListItem
+        For Each item In CheckBoxList1.Items
+            If item.Selected = True Then
+                additionalCharges += item.Value
+            End If
+        Next
+
+        'calculate total cost
+        totalCost = mileageCost + additionalCharges + (ts.TotalDays * pricePerDay)
+
+        'display to textbox the subtotal and total cost for the invoice
+        TextBox3.Text = "Daily cost of vehicle for " & ts.TotalDays.ToString & " days is: $" &
+            pricePerDay * ts.TotalDays & vbNewLine &
+            "Mileage charges: $" & mileageCost & vbNewLine &
+            "Additional charges: $" & additionalCharges & vbNewLine &
+            "Total cost: $" & totalCost
+
+
+        'invoice ledger data table
+        Dim dr As DataRow = gdtRentals.NewRow
+
+        dr.Item("Checkout Date") = begDate.ToShortDateString
+        dr.Item("Checkin Date") = endDate.ToShortDateString
+        dr.Item("Checkout Mileage") = TextBox4.Text
+        dr.Item("Checkin Mileage") = TextBox5.Text
+        dr.Item("Vehicle Type") = DropDownList1.SelectedValue
+        dr.Item("Total Charges") = totalCost
+        dr.Item("Reservation Total Count") += 1
+
+        gdtRentals.Rows.Add(dr)
+        GridView1.DataSource = gdtRentals
+        GridView1.DataBind()
+
+
+
+        'individual vehicle data table
+        With individual.Rows(DropDownList1.SelectedIndex)
+            .Item("Current Mileage") = TextBox5.Text
+
+            If (TextBox5.Text > 10000) Then
+                .Item("Needs Maintainence") = True
+            End If
+        End With
+        'set data table to the gridview
+        GridView2.DataSource = individual
+        GridView2.DataBind()
+
+
+
+        ' grand total table
+        With grandTotal.Rows(0)
+            .Item("Total Times Rented") += 1
+            .Item("Total Days Rented") += ts.TotalDays
+            .Item("Total Miles Traveled") += totalMileage
+            .Item("Total Revenue Generated") += totalCost
+        End With
+        ' set to gridview
+        GridView3.DataSource = grandTotal
+        GridView3.DataBind()
+
+
+
+        'vehicle type table
+        ' val is used to index between each vehicle type in drop down list1
+        Dim val As Integer
+        If (DropDownList1.SelectedValue = "Sedan1" Or DropDownList1.SelectedValue = "Sedan2" Or DropDownList1.SelectedValue = "Sedan3") Then
+            val = 0
+        ElseIf (DropDownList1.SelectedValue = "Van1" Or DropDownList1.SelectedValue = "Van2") Then
+            val = 1
+        ElseIf (DropDownList1.SelectedValue = "Pickup1" Or DropDownList1.SelectedValue = "Pickup2") Then
+            val = 2
+        End If
+
+        With vehicleType.Rows(val)
+            .Item("Total Mileage") += totalMileage
+        End With
+        'set to gridview
+        GridView4.DataSource = vehicleType
+        GridView4.DataBind()
+
+    End Sub
+End Class
